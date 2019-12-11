c9536e93fd596860e1323d15c590adff7093755f
zhangcn <15236674712@163.com>
2019-12-11 08:49:27 +0800

    开发者添加测试类main

--- src/test/java/com/baizhi/testGit.java
+++ src/test/java/com/baizhi/testGit.java
@@ -11,6 +11,11 @@ public class testGit {
     public static void main(String[] args) {
 
         System.out.println("============");
+        System.out.println("============");
+        System.out.println("============");
+        System.out.println("============");
+        System.out.println("============");
+        System.out.println("============");
     }
 
 }
